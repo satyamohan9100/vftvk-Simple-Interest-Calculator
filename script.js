d44db99ed9d530da218f9b36d6767fb1f87cd895
satyamohan <78737500+satyamohan9100@users.noreply.github.com>
2022-01-23 23:38:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var t = document.getElementById("years").value;
+    var r = document.getElementById("rate").value;
+
+    var simpleInterest = ((p*t*r)/100);
+   
+   document.getElementById("result").innerHTML = "<br>If you deposit <mark>"+ p+"</mark>,</br>"
+                                                +"at an interest rate of <mark>"+r+"%</mark>.</br>"
+                                                +"You will receive an amount of <mark>"+simpleInterest+"</mark>,</br>"
+                                                +"in the year <mark>"+202+t+"</mark>";
+   
 }
-        
