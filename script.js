c0e90ff14b4b7a49d909b7fceede79cc60012c2d
satyamohan <78737500+satyamohan9100@users.noreply.github.com>
2022-01-24 00:33:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function compute()
 {
-    var p = document.getElementById("principal").value;
-    var t = document.getElementById("years").value;
-    var r = document.getElementById("rate").value;
+    var principal = document.getElementById("principal").value;
+    var years = document.getElementById("years").value;
+    var rate = document.getElementById("rate").value;
 
-    var simpleInterest = ((p*t*r)/100);
+    var simpleInterest = ((principal*years*rate)/100);
+    var year = new Date().getFullYear()+parseInt(years);
    
-   document.getElementById("result").innerHTML = "<br>If you deposit <mark>"+ p+"</mark>,</br>"
-                                                +"at an interest rate of <mark>"+r+"%</mark>.</br>"
+   document.getElementById("result").innerHTML = "<br>If you deposit <mark>"+ principal+"</mark>,</br>"
+                                                +"at an interest rate of <mark>"+rate+"%</mark>.</br>"
                                                 +"You will receive an amount of <mark>"+simpleInterest+"</mark>,</br>"
-                                                +"in the year <mark>"+202+t+"</mark>";
+                                                +"in the year <mark>"+year+"</mark>";
    
 }
