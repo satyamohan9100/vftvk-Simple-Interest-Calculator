2d1ac5240b51d9abc49ca3613d8d3f9bae6fd3f9
satyamohan <78737500+satyamohan9100@users.noreply.github.com>
2022-01-24 12:53:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    if (principal == 0 || isNaN(principal) || principal < 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+    location.reload();
+    return false;
+  }
     var years = document.getElementById("years").value;
     var rate = document.getElementById("rate").value;
 
